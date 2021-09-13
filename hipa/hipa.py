93210e7512bcb44f8278bbdacda230f03054116e
naoki <naoki.highbridge@gmail.com>
2021-09-13 12:23:57 +0900

    modify

--- hipa/hipa.py
+++ hipa/hipa.py
@@ -1,2 +1,25 @@
+
 def hello():
-	print("hello	")
+	print("hello")
+
+
+
+import os
+from mit_semseg.config import cfg
+
+def make_cfg(cfg_file, cfg_list):
+
+	cfg.merge_from_file(cfg_file)
+	cfg.merge_from_list(cfg_list)
+	
+	cfg.MODEL.arch_encoder = cfg.MODEL.arch_encoder.lower()
+	cfg.MODEL.arch_decoder = cfg.MODEL.arch_decoder.lower()
+	
+	# absolute paths of model weights
+	cfg.MODEL.weights_encoder = os.path.join(cfg.DIR, 'encoder_' + cfg.TEST.checkpoint)
+	cfg.MODEL.weights_decoder = os.path.join(cfg.DIR, 'decoder_' + cfg.TEST.checkpoint)
+	
+	cfg.list_test = [{'fpath_img': x} for x in ["ADE_val_00001519.jpg"]]
+	
+	if not os.path.isdir(cfg.TEST.result):
+	    os.makedirs(cfg.TEST.result)

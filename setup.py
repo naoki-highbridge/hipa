0f547c749b2d57001549c26bc549cab25e7a31be
naoki <naoki.highbridge@gmail.com>
2021-09-13 12:13:07 +0900

    modify

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup, find_packages
 
 setuptools.setup(
     name="hipa",
     version="0.1.0",
-    author="naoki-highbridge",
-    description="Home interior picture analyzer",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
+    packages=find_packages(),
 )
